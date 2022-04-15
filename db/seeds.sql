a8d4774eddfa201d44716fd8cdbe919d47d07e98
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-04-15 17:36:30 -0500

    seeds dont populate correctly

--- db/seeds.sql
+++ db/seeds.sql
@@ -32,26 +32,26 @@ VALUES
     ("Office Administrator", 41500, 1);
 
 INSERT INTO
-    employees(first_name, last_name, role_id, manager_id)
+    employees(id, first_name, last_name, role_id, manager_id)
 VALUES
-    ("Creed", "Bratton", 9, 4),
-    ("Stanley", "Hudson", 11, 14),
-    ("Phyllis", "Lapin-Vance", 12, 14),
-    ("Michael", "Scott", 1, null),
-    ("Dwight", "Schrute", 11, 14),
-    ("Meredith", "Palmer", 7, 4),
-    ("Toby", "Flenderson", 3, null),
-    ("Angela", "Martin", 4, 4),
-    ("Oscar", "Martinez", 5, 8),
-    ("Darryl", "Philbin", 14, 4),
-    ("Andy", "Bernard", 12, 14),
-    ("Lonnis", "Collins", 15, 10),
-    ("Kevin", "Malone", 6, 8),
-    ("Jim", "Halpert", 11, 4),
-    ("Kelly", "Kapoor", 8, 4),
-    ("Madge", "Madsen", 15, 10),
-    ("Pam", "Halpert", 16, 4),
-    ("Roy", "Anderson", 15, 10),
-    ("Jerry", "DiCanio", 15, 10),
-    ("Ryan", "Howard", 13, 14),
-    ("Kelly Erin", "Hannon", 10, 17);
+    (4, "Michael", "Scott", 1, null),
+    (7, "Toby", "Flenderson", 3, null),
+    (1, "Creed", "Bratton", 9, 4),
+    (2, "Stanley", "Hudson", 11, 14),
+    (3, "Phyllis", "Lapin-Vance", 12, 14),
+    (5, "Dwight", "Schrute", 11, 14),
+    (6, "Meredith", "Palmer", 7, 4),
+    (8, "Angela", "Martin", 4, 4),
+    (9, "Oscar", "Martinez", 5, 8),
+    (10, "Darryl", "Philbin", 14, 4),
+    (11, "Andy", "Bernard", 12, 14),
+    (12, "Lonnis", "Collins", 15, 10),
+    (13, "Kevin", "Malone", 6, 8),
+    (14, "Jim", "Halpert", 11, 4),
+    (15, "Kelly", "Kapoor", 8, 4),
+    (16, "Madge", "Madsen", 15, 10),
+    (17, "Pam", "Halpert", 16, 4),
+    (18, "Roy", "Anderson", 15, 10),
+    (19, "Jerry", "DiCanio", 15, 10),
+    (20, "Ryan", "Howard", 13, 14),
+    (21, "Kelly Erin", "Hannon", 10, 17);
