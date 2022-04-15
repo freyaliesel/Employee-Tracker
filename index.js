b0d47defacbb20d9f055169d538674004eb0d9d3
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-04-15 17:36:16 -0500

    basic outline of functions

---
+++ index.js
@@ -0,0 +1,17 @@
+// prompt user for input - should be recursive
+
+// A view all departments
+// B view all roles
+// C view all employees
+// D add a department
+// E add a role
+// F add an employee
+// G update employee role
+
+// A prints a table to console with department names and ids
+// B prints a table to console with job title, role id, department it belongs to, and salary
+// C prints a table to console with employee id, first and last names, job title, department, salary, and manager they report to (if applicable)
+// D prompts user to enter name of department - creates new department in database
+// E prompts user to enter role name, salary, and department, creates new role in database
+// F prompts user to enter first and last names, role, and manager, creates new employee in database
+// G prompts user to select an employee to update, enter their new role
